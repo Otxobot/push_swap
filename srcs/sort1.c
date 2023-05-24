3b7734178e5efea0e3873da2748b43e775f9ac73
Otxobot <otxoboy64@gmail.com>
2023-05-24 16:04:26 +0200

    lines shortened in sort1.c

--- srcs/sort1.c
+++ srcs/sort1.c
@@ -6,7 +6,7 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 12:48:50 by abasante          #+#    #+#             */
-/*   Updated: 2023/05/24 15:51:51 by abasante         ###   ########.fr       */
+/*   Updated: 2023/05/24 16:03:48 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,33 +28,43 @@ void	easy_sort(t_list **a, t_list **b, int size)
 	else if (size == 3)
 	{
 		//case 1 sa:
-		if (((*a)->number > (*a)->next->number) && ((*a)->next->number < (*a)->next->next->number) && ((*a)->number < (*a)->next->next->number) )
+		if (((*a)->number > (*a)->next->number) && \
+		((*a)->next->number < (*a)->next->next->number) && \
+		((*a)->number < (*a)->next->next->number))
 		{
 			printf("case 1:\n");
 			sa(a);
 		}
 		//case 2:
-		else if (((*a)->number > (*a)->next->number) && ((*a)->next->number > (*a)->next->next->number) && ((*a)->number > (*a)->next->next->number))
+		else if (((*a)->number > (*a)->next->number) && \
+		((*a)->next->number > (*a)->next->next->number) && \
+		((*a)->number > (*a)->next->next->number))
 		{
 			printf("case 2:\n");
 			sa(a);
 			rra(a);
 		}
 		//case 3:
-		else if (((*a)->number > (*a)->next->number) && ((*a)->number > (*a)->next->next->number) && ((*a)->next->number < (*a)->next->next->number))
+		else if (((*a)->number > (*a)->next->number) && \
+		((*a)->number > (*a)->next->next->number) && \
+		((*a)->next->number < (*a)->next->next->number))
 		{
 			printf("case 3:\n");
 			ra(a);
 		}
 		//case 4:
-		else if (((*a)->next->number > (*a)->number) && ((*a)->next->number > (*a)->next->next->number) && ((*a)->number < (*a)->next->next->number))
+		else if (((*a)->next->number > (*a)->number) && \
+		((*a)->next->number > (*a)->next->next->number) && \
+		((*a)->number < (*a)->next->next->number))
 		{
 			printf("case 4:\n");
 			sa(a);
 			ra(a);
 		}
 		//case 5:
-		else if (((*a)->next->next->number < (*a)->number) && ((*a)->next->next->number < (*a)->next->number) && ((*a)->number > (*a)->next->next->number))
+		else if (((*a)->next->next->number < (*a)->number) && \
+		((*a)->next->next->number < (*a)->next->number) && \
+		((*a)->number > (*a)->next->next->number))
 		{
 			printf("case 5:\n");
 			rra(a);
