277a0a302813e2ec5baf6aa8a5b10848c8738833
Otxobot <otxoboy64@gmail.com>
2023-05-29 13:26:48 +0200

    doesn't correctly organize 5 numbers

--- srcs/sort2.c
+++ srcs/sort2.c
@@ -6,45 +6,29 @@
 /*   By: abasante <abasante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 11:12:21 by abasante          #+#    #+#             */
-/*   Updated: 2023/05/29 13:08:08 by abasante         ###   ########.fr       */
+/*   Updated: 2023/05/29 13:24:16 by abasante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/push_swap.h"
-
-void	three_numbers(t_list **a)
-{
-	t_list	*first;
-	t_list	*second;
-	t_list	*third;
-
-	while(check_order(a, 'a') == 1)
-	{
-		first = (*a);
-		second = (*a)->next;
-		third = (*a)->next->next;
-
-		if (first->number > second->number && first->number < third->number)
-			sa(a);
-		else if (first->number > second->number && first->number > third->number)
-			ra(a);
-		else if (first->number < second->number && first->number > third->number)
-			rra(a);
-		else if (first->number < second->number && first->number < third->number)
-			sa(a);
-	}
-}
 
 void	medium_sort(t_list **a, t_list **b, int size)
 {
 	size = 0;
 	printLinkedList(*a);
 	pa(a, b);
+	write (1, "pa\n", 3);
 	pa(a, b);
+	write (1, "pa\n", 3);
 	sa(a);
+	write(1, "sa\n", 3);
 	ra(a);
+	write (1, "ra\n", 3);
 	pa(b, a);
+	write (1, "pa\n", 3);
 	ra(a);
+	write (1, "ra\n", 3);
 	pa(b, a);
+	write (1, "pa\n", 3);
 	printLinkedList(*a);
 }
